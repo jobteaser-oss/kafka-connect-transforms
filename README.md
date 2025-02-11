7320557f894f2f6d94b9ed8b6690d3b1ee61b6e5
Jeremy Guiselin <jeremy.guiselin@jobteaser.com>
2025-02-11 17:22:12 +0100

    feat: add joinfields smt

--- README.md
+++ README.md
@@ -19,3 +19,46 @@ transforms="obfuscate-email"
 transforms.obfuscate-email.type="com.jobteaser.kafka.connect.transforms.ObfuscateEmail$Value"
 transforms.obfuscate-email.email.field.name="email"
 ```
+
+## Join fields
+
+Kafka Connect SMT to join different values into a new one using a separator
+
+This SMT supports joining values into the record Key or Value
+
+Properties:
+
+| Name                      | Description                                              | Type   | Default    | Importance |
+|---------------------------|----------------------------------------------------------|--------|------------|------------|
+| `join-fields.fields`      | List of the fields keys to join into the destination key | List   | empty list | High       |
+| `join-fields.separator`   | Separator of the fields values joined                    | String | .          | High       |
+| `join-fields.destination` | Key name of the joined field                             | String | output     | High       |
+
+
+Example on how to add to your connector:
+```
+transforms="generateTableFullName"
+transforms.generateTableFullName.type="com.jobteaser.kafka.connect.transforms.JoinFields$Value"
+transforms.generateTableFullName.join-fields.fields="__source_db,__source_table"
+transforms.generateTableFullName.join-fields.separator="."
+transforms.generateTableFullName.join-fields.destination="fully_qualified_name"
+```
+
+Before:
+```json
+{
+  "__source_db": "my_database", 
+  "__source_table": "my_table", 
+  "id": 123
+}
+```
+
+After:
+```json
+{
+  "__source_db": "my_database", 
+  "__source_table": "my_table", 
+  "fully_qualified_name": "my_database.my_table", 
+  "id": 123
+}
+```
---
+++ src/test/java/com/jobteaser/kafka/connect/transforms/JoinFieldsTest.java
@@ -0,0 +1,153 @@
+package com.jobteaser.kafka.connect.transforms;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JoinFieldsTest {
+    private final JoinFields<SourceRecord> smt = new JoinFields.Value<>();
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        smt.close();
+    }
+
+    @Test
+    public void topLevelStructRequired() {
+        assertThrows(DataException.class, () -> {
+            smt.configure(Collections.emptyMap());
+            smt.apply(new SourceRecord(null, null, "", 0, Schema.INT32_SCHEMA, 42));
+        });
+    }
+
+    @Test
+    public void schemaDefaultConfigNoElement() {
+        final Map<String, Object> props = new HashMap<>();
+        smt.configure(props);
+
+        final Schema initialStructSchema =
+                SchemaBuilder.struct().name("name").version(1).doc("doc")
+                        .field("abc", Schema.STRING_SCHEMA)
+                        .build();
+        final Struct initialStruct = new Struct(initialStructSchema)
+                .put("abc", "def");
+
+
+        final Schema expectedStructSchema =
+                SchemaBuilder.struct().name("name").version(1).doc("doc")
+                        .field("abc", Schema.STRING_SCHEMA)
+                        .field("output", Schema.STRING_SCHEMA)
+                        .build();
+        final Struct expectedStruct = new Struct(expectedStructSchema)
+                .put("abc", "def")
+                .put("output", "");
+
+
+        final SourceRecord record = new SourceRecord(null, null, "test", 0, initialStructSchema, initialStruct);
+        final SourceRecord transformedRecord = smt.apply(record);
+
+        assertEquals(expectedStructSchema, transformedRecord.valueSchema());
+        assertEquals(expectedStruct, transformedRecord.value());
+    }
+
+    @Test
+    public void schemalessDefaultConfigNoElement() {
+        final Map<String, Object> props = new HashMap<>();
+        smt.configure(props);
+
+        final Map<String, Object> initialMap = new HashMap<>();
+        initialMap.put("abc", "def");
+
+        final Map<String, Object> expectedMap = new HashMap<>();
+        expectedMap.put("abc", "def");
+        expectedMap.put("output", "");
+
+        final SourceRecord record = new SourceRecord(null, null, "test", 0, null, initialMap);
+        final SourceRecord transformedRecord = smt.apply(record);
+
+        assertNull(transformedRecord.valueSchema());
+        assertEquals(expectedMap, transformedRecord.value());
+    }
+
+    @Test
+    public void schemaJoinDifferentTypes() {
+        final Map<String, Object> props = new HashMap<>();
+        props.put("join-fields.fields", Arrays.asList("test_str", "test_int", "test_bool", "test_float"));
+        props.put("join-fields.destination", "opt");
+        props.put("join-fields.separator", "_");
+        smt.configure(props);
+
+        final Schema initialStructSchema =
+                SchemaBuilder.struct().name("name").version(1).doc("doc")
+                        .field("test_str", Schema.STRING_SCHEMA)
+                        .field("test_int", Schema.INT32_SCHEMA)
+                        .field("test_bool", Schema.BOOLEAN_SCHEMA)
+                        .field("test_float", Schema.FLOAT64_SCHEMA)
+                        .build();
+        final Struct initialStruct = new Struct(initialStructSchema)
+                .put("test_str", "abc")
+                .put("test_int", 123)
+                .put("test_float", 456.78)
+                .put("test_bool", false);
+
+        final Schema expectedStructSchema =
+                SchemaBuilder.struct().name("name").version(1).doc("doc")
+                        .field("test_str", Schema.STRING_SCHEMA)
+                        .field("test_int", Schema.INT32_SCHEMA)
+                        .field("test_bool", Schema.BOOLEAN_SCHEMA)
+                        .field("test_float", Schema.FLOAT64_SCHEMA)
+                        .field("opt", Schema.STRING_SCHEMA)
+                        .build();
+        final Struct expectedStruct = new Struct(expectedStructSchema)
+                .put("test_str", "abc")
+                .put("test_int", 123)
+                .put("test_float", 456.78)
+                .put("test_bool", false)
+                .put("opt", "abc_123_false_456.78");
+
+        final SourceRecord record = new SourceRecord(null, null, "test", 0, initialStructSchema, initialStruct);
+        final SourceRecord transformedRecord = smt.apply(record);
+
+        assertEquals(expectedStructSchema, transformedRecord.valueSchema());
+        assertEquals(expectedStruct, transformedRecord.value());
+    }
+
+    @Test
+    public void schemalessJoinDifferentTypes() {
+        final Map<String, Object> props = new HashMap<>();
+        props.put("join-fields.fields", Arrays.asList("test_str", "test_int", "test_bool", "test_float"));
+        props.put("join-fields.destination", "opt");
+        props.put("join-fields.separator", "_");
+        smt.configure(props);
+
+        final Map<String, Object> initialMap = new HashMap<>();
+        initialMap.put("test_str", "abc");
+        initialMap.put("test_int", 123);
+        initialMap.put("test_float", 456.78);
+        initialMap.put("test_bool", false);
+
+        final Map<String, Object> expectedMap = new HashMap<>();
+        expectedMap.put("test_str", "abc");
+        expectedMap.put("test_int", 123);
+        expectedMap.put("test_float", 456.78);
+        expectedMap.put("test_bool", false);
+        expectedMap.put("opt", "abc_123_false_456.78");
+
+        final SourceRecord record = new SourceRecord(null, null, "test", 0, null, initialMap);
+        final SourceRecord transformedRecord = smt.apply(record);
+
+        assertNull(transformedRecord.valueSchema());
+        assertEquals(expectedMap, transformedRecord.value());
+    }
+}
